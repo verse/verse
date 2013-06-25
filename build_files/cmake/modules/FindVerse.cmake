a0f4e01120f4434a579bcce12bc909e02a494b9b
Jiri Hnidek <jiri.hnidek@tul.cz>
2013-06-25 10:19:21 +0200

    * The Verse server is linked with libWebsockets library  - Added basic cmake find module for libWebsockets  - Modified CMakeLists.txt to have optional support for libWebsockets

--- build_files/cmake/modules/FindVerse.cmake
+++ build_files/cmake/modules/FindVerse.cmake
@@ -1,6 +1,7 @@
 # This module tries to find Verse library and include files
 #
 # VERSE_INCLUDE_DIR, where to find verse.h
+# VERSE_LIBRARY_DIR, where to find libverse.so
 # VERSE_LIBRARIES, the library to link against
 # VERSE_FOUND, IF false, do not try to use Verse
 #
@@ -16,9 +17,17 @@ FIND_LIBRARY ( VERSE_LIBRARIES verse
     /usr/lib
 )
 
+GET_FILENAME_COMPONENT( VERSE_LIBRARY_DIR ${VERSE_LIBRARIES} PATH )
+
 SET ( VERSE_FOUND "NO" )
 IF ( VERSE_INCLUDE_DIR )
     IF ( VERSE_LIBRARIES )
         SET ( VERSE_FOUND "YES" )
     ENDIF ( VERSE_LIBRARIES )
 ENDIF ( VERSE_INCLUDE_DIR )
+
+MARK_AS_ADVANCED(
+    VERSE_LIBRARY_DIR
+    VERSE_INCLUDE_DIR
+    VERSE_LIBRARIES
+)
