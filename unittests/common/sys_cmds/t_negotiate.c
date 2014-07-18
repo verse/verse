9aada6d904d3fe10e10285389d93bbfe960ecb24
Jiri Hnidek <jiri.hnidek@tul.cz>
2014-07-18 21:53:00 +0200

    Refactoring and added another simple test.
    It tests packing/unpacking negotiate command with multiple
    uint8 values.

--- unittests/common/sys_cmds/t_negotiate.c
+++ unittests/common/sys_cmds/t_negotiate.c
@@ -126,10 +126,10 @@ START_TEST ( test_add_negotiate_cmd_multiple_values )
 			"Negotiate command feature count: %d != %d",
 			sys_cmd->negotiate_cmd.count, 2);
 	fail_unless( sys_cmd->negotiate_cmd.value[0].uint8 == values[0],
-			"Negotiate command value: %d != %d",
+			"Negotiate command value[0]: %d != %d",
 			sys_cmd->negotiate_cmd.value[0].uint8, values[0]);
 	fail_unless( sys_cmd->negotiate_cmd.value[1].uint8 == values[1],
-			"Negotiate command value: %d != %d",
+			"Negotiate command value[1]: %d != %d",
 			sys_cmd->negotiate_cmd.value[1].uint8, values[1]);
 
 }
@@ -149,17 +149,20 @@ START_TEST ( test_pack_unpack_negotiate_cmd_single_value )
 	int buffer_pos = 0, cmd_len;
 
 	/* Create negotiate command */
-	v_add_negotiate_cmd(send_sys_cmd, 0, cmd_op_code, ftr_op_code, &value, NULL);
+	v_add_negotiate_cmd(send_sys_cmd, 0, cmd_op_code, ftr_op_code,
+			&value, NULL);
 
 	/* Pack negotiate command */
-	buffer_pos += v_raw_pack_negotiate_cmd(buffer, &send_sys_cmd[0].negotiate_cmd);
+	buffer_pos += v_raw_pack_negotiate_cmd(buffer,
+			&send_sys_cmd[0].negotiate_cmd);
 
 	fail_unless( buffer_pos == 4,
 			"Length of packed cmd: %d != %d",
 			buffer_pos, 4);
 
 	/* Unpack system command */
-	cmd_len = v_raw_unpack_negotiate_cmd(buffer, buffer_pos, &recv_sys_cmd[0].negotiate_cmd);
+	cmd_len = v_raw_unpack_negotiate_cmd(buffer, buffer_pos,
+			&recv_sys_cmd[0].negotiate_cmd);
 
 	fail_unless( cmd_len == buffer_pos,
 			"Length of packed and unpacked cmd: %d != %d",
@@ -181,6 +184,55 @@ END_TEST
 
 
 /**
+ * \brief Test packing and unpacking of negotiate command with multiple values
+ */
+START_TEST ( test_pack_unpack_negotiate_cmd_multiple_values )
+{
+	union VSystemCommands send_sys_cmd[1], recv_sys_cmd[1];
+	uint8 cmd_op_code = CMD_CHANGE_L_ID;
+	uint8 ftr_op_code = FTR_FC_ID;
+	uint8 values[2] = {FC_TCP_LIKE, FC_NONE};
+	char buffer[255];
+	int buffer_pos = 0, cmd_len;
+
+	/* Create negotiate command */
+	v_add_negotiate_cmd(send_sys_cmd, 0, cmd_op_code, ftr_op_code,
+			&values[0], &values[1], NULL);
+
+	/* Pack negotiate command */
+	buffer_pos += v_raw_pack_negotiate_cmd(buffer,
+			&send_sys_cmd[0].negotiate_cmd);
+
+	fail_unless( buffer_pos == 5,
+			"Length of packed cmd: %d != %d",
+			buffer_pos, 5);
+
+	/* Unpack system command */
+	cmd_len = v_raw_unpack_negotiate_cmd(buffer, buffer_pos,
+			&recv_sys_cmd[0].negotiate_cmd);
+
+	fail_unless( cmd_len == buffer_pos,
+			"Length of packed and unpacked cmd: %d != %d",
+			buffer_pos, cmd_len);
+	fail_unless( recv_sys_cmd->negotiate_cmd.id == cmd_op_code,
+			"Negotiate command OpCode: %d != %d",
+			recv_sys_cmd->negotiate_cmd.id, cmd_op_code);
+	fail_unless( recv_sys_cmd->negotiate_cmd.feature == ftr_op_code,
+			"Negotiate command feature: %d != %d",
+			recv_sys_cmd->negotiate_cmd.feature, ftr_op_code);
+	fail_unless( recv_sys_cmd->negotiate_cmd.count == 2,
+			"Negotiate command feature count: %d != %d",
+			recv_sys_cmd->negotiate_cmd.count, 2);
+	fail_unless( recv_sys_cmd->negotiate_cmd.value[0].uint8 == values[0],
+			"Negotiate command value[0]: %d != %d",
+			recv_sys_cmd->negotiate_cmd.value[0].uint8, values[0]);
+	fail_unless( recv_sys_cmd->negotiate_cmd.value[1].uint8 == values[1],
+			"Negotiate command value[1]: %d != %d",
+			recv_sys_cmd->negotiate_cmd.value[1].uint8, values[1]);
+}
+END_TEST
+
+/**
  * \brief This function creates test suite for Node_Create command
  */
 struct Suite *negotiate_suite(void)
@@ -191,6 +243,7 @@ struct Suite *negotiate_suite(void)
 	tcase_add_test(tc_core, test_add_negotiate_cmd_single_value);
 	tcase_add_test(tc_core, test_add_negotiate_cmd_multiple_values);
 	tcase_add_test(tc_core, test_pack_unpack_negotiate_cmd_single_value);
+	tcase_add_test(tc_core, test_pack_unpack_negotiate_cmd_multiple_values);
 
 	suite_add_tcase(suite, tc_core);
 
