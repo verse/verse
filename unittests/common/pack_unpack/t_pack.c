1167e1dda9502ccdefc695a9f4af694386fb392f
Jiri Hnidek <jiri.hnidek@tul.cz>
2014-10-30 09:55:11 +0100

    Added more tests, fix comments, small bugs and typos.

--- unittests/common/pack_unpack/t_pack.c
+++ unittests/common/pack_unpack/t_pack.c
@@ -30,6 +30,9 @@
 
 #define UINT8_BUF_SIZE		8
 #define UINT16_BUF_SIZE		16
+#define UINT32_BUF_SIZE		32
+#define UINT64_BUF_SIZE		64
+#define REAL32_BUF_SIZE		32
 #define REAL64_BUF_SIZE		64
 
 
@@ -66,15 +69,15 @@ START_TEST ( test_Pack_Uint8 )
 
 	for(i = 0; i < UINT8_BUF_SIZE; i++) {
 		fail_unless( buffer[i] == expected_results[i],
-			"Buffer of uint8 differs at position: %d (%d != %d)",
-			i, buffer[i], expected_results[i]);
+				"Buffer of uint8 differs at position: %d (%d != %d)",
+				i, buffer[i], expected_results[i]);
 	}
 }
 END_TEST
 
 
 /**
- * \brief Unit test of packing uint8 values
+ * \brief Unit test of packing uint16 values
  */
 START_TEST ( test_Pack_Uint16 )
 {
@@ -96,14 +99,90 @@ START_TEST ( test_Pack_Uint16 )
 	buf_pos += vnp_raw_pack_uint16((void*)&buffer[buf_pos], 1030);
 	buf_pos += vnp_raw_pack_uint16((void*)&buffer[buf_pos], 65535);
 
-	fail_unless( buf_pos == 10,
+	fail_unless( buf_pos == 5*2,
 			"Size of uint16 buffer: %d != 10",
 			buf_pos);
 
 	for(i = 0; i < UINT16_BUF_SIZE; i++) {
 		fail_unless( buffer[i] == expected_results[i],
-			"Buffer of uint16 differs at position: %d (%d != %d)",
-			i, buffer[i], expected_results[i]);
+				"Buffer of uint16 differs at position: %d (%d != %d)",
+				i, buffer[i], expected_results[i]);
+	}
+}
+END_TEST
+
+
+/**
+ * \brief Unit test of packing uint32 values
+ */
+START_TEST ( test_Pack_Uint32 )
+{
+	size_t buf_pos = 0;
+	unsigned char buffer[UINT32_BUF_SIZE] = {0, };
+	unsigned char expected_results[UINT32_BUF_SIZE] = {
+			0x00, 0x00, 0x00, 0x00,	/* 0 */
+			0x00, 0x00, 0x00, 0x01,	/* 1 */
+			0x00, 0x00, 0x02, 0x00,	/* 512 */
+			0x00, 0x01, 0x00, 0x04,	/* 65540 */
+			0x01, 0x00, 0x00, 0x00,	/* 16 777 216 */
+			0xff, 0xff, 0xff, 0xff,	/* 4 294 967 295 */
+			0,
+	};
+	int i;
+
+	buf_pos += vnp_raw_pack_uint32((void*)&buffer[buf_pos], 0);
+	buf_pos += vnp_raw_pack_uint32((void*)&buffer[buf_pos], 1);
+	buf_pos += vnp_raw_pack_uint32((void*)&buffer[buf_pos], 512);
+	buf_pos += vnp_raw_pack_uint32((void*)&buffer[buf_pos], 65540);
+	buf_pos += vnp_raw_pack_uint32((void*)&buffer[buf_pos], 16777216);
+	buf_pos += vnp_raw_pack_uint32((void*)&buffer[buf_pos], 4294967295);
+
+	fail_unless( buf_pos == 6*4,
+			"Size of uint32 buffer: %d != 24",
+			buf_pos);
+
+	for(i = 0; i < UINT32_BUF_SIZE; i++) {
+		fail_unless( buffer[i] == expected_results[i],
+				"Buffer of uint32 differs at position: %d (%d != %d)",
+				i, buffer[i], expected_results[i]);
+	}
+}
+END_TEST
+
+
+/**
+ * \brief Unit test of packing uint64 values
+ */
+START_TEST ( test_Pack_Uint64 )
+{
+	size_t buf_pos = 0;
+	unsigned char buffer[UINT64_BUF_SIZE] = {0, };
+	unsigned char expected_results[UINT64_BUF_SIZE] = {
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	/* 0 */
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,	/* 1 */
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00,	/* 512 */
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x04,	/* 65540 */
+			0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00,	/* 4 294 967 296 */
+			0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,	/* 18 446 744 073 709 551 615 */
+			0,
+	};
+	int i;
+
+	buf_pos += vnp_raw_pack_uint64((void*)&buffer[buf_pos], 0);
+	buf_pos += vnp_raw_pack_uint64((void*)&buffer[buf_pos], 1);
+	buf_pos += vnp_raw_pack_uint64((void*)&buffer[buf_pos], 512);
+	buf_pos += vnp_raw_pack_uint64((void*)&buffer[buf_pos], 65540);
+	buf_pos += vnp_raw_pack_uint64((void*)&buffer[buf_pos], 4294967296);
+	buf_pos += vnp_raw_pack_uint64((void*)&buffer[buf_pos], 0xffffffffffffffff);
+
+	fail_unless( buf_pos == 6*8,
+			"Size of uint64 buffer: %d != 48",
+			buf_pos);
+
+	for(i = 0; i < UINT64_BUF_SIZE; i++) {
+		fail_unless( buffer[i] == expected_results[i],
+				"Buffer of uint64 differs at position: %d (%d != %d)",
+				i, buffer[i], expected_results[i]);
 	}
 }
 END_TEST
@@ -132,13 +211,13 @@ START_TEST ( test_Pack_Real64 )
 	buf_pos += vnp_raw_pack_real64((void*)&buffer[buf_pos], 1.0/3.0);
 
 	fail_unless( buf_pos == 5*8,
-			"Size of real64 buffer: %d != 16 (bytes)",
+			"Size of real64 buffer: %d != 40 (bytes)",
 			buf_pos);
 
 	for(i = 0; i < REAL64_BUF_SIZE; i++) {
 		fail_unless( buffer[i] == expected_results[i],
-							"Buffer of real64 differs at position: %d",
-							i);
+				"Buffer of real64 differs at position: %d",
+				i);
 	}
 }
 END_TEST
@@ -153,6 +232,8 @@ struct Suite *pack_suite(void)
 
 	tcase_add_test(tc_core, test_Pack_Uint8);
 	tcase_add_test(tc_core, test_Pack_Uint16);
+	tcase_add_test(tc_core, test_Pack_Uint32);
+	tcase_add_test(tc_core, test_Pack_Uint64);
 	tcase_add_test(tc_core, test_Pack_Real64);
 
 	suite_add_tcase(suite, tc_core);
